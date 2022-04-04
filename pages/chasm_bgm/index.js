a745df108037c6949985e326808c36a338db7981
Guyutongxue <guyutongxue@163.com>
2022-04-04 13:25:04 +0800

    update

--- pages/chasm_bgm/index.js
+++ pages/chasm_bgm/index.js
@@ -20,7 +20,9 @@ function generateHowl(/** @type {string[]} */ name, /** @type {number} */ no) {
 /** @type {Howl[][]} */
 const AUDIOS = [
     [
-        "1.b.mp3"
+        "1.b.mp3",
+        "1.b.mp3", // TODO
+        "1.b.mp3",
     ],
     [
         "2.b.mp3",
@@ -39,6 +41,7 @@ const AUDIOS = [
     ],
     [
         "5.b.mp3", 
+        "5.mp3",
         "5.mp3"
     ],
 ].map((name, no) => generateHowl(name, no));
@@ -49,9 +52,9 @@ const AUDIO_LOADED = AUDIOS.flat().map(a => new Promise(r => a.on('load', r)));
 
 async function play(no, ty = 0) {
     await AUDIO_LOADED;
+    const id = AUDIOS[no][ty].play('d');
     const now = (new Date()).getTime();
     const tick = now % (END[no] - START[no]);
-    const id = AUDIOS[no][ty].play('d');
     AUDIOS[no][ty].seek(tick / 1000, id);
     // Stop other audios
     AUDIOS[no].filter((_, i) => i !== ty).forEach(a => a.stop());
@@ -60,4 +63,33 @@ async function play(no, ty = 0) {
 
 function stop() {
     AUDIOS.forEach(a => a.forEach(b => b.stop()));
+}
+
+let place = 1;
+let ty = -1;
+
+function tySwitch(newTy) {
+    const srcEle = document.querySelector(`.switch-btn[data-ty="${newTy}"]`);
+    if (srcEle.classList.contains("active")) {
+        console.log("stop");
+        srcEle.classList.remove("active");
+        srcEle.blur();
+        ty = -1;
+        stop();
+    } else {
+        document.querySelectorAll(".switch-btn").forEach(e => e.classList.remove("active"));
+        srcEle.classList.add("active");
+        ty = newTy;
+        play(place, ty);
+    }
+}
+
+function placeSwitch(newPlace) {
+    place = newPlace;
+    if (ty !== -1) {
+        play(place, ty);
+    }
+    document.querySelectorAll(".point").forEach(e => e.classList.remove("active"));
+    const srcEle = document.querySelector(`.point[data-place="${newPlace}"]`);
+    srcEle.classList.add("active");
 }
